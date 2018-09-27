99471bf023bddb97eb118df3f900ef8547247dfb
alynielson <nielsonaly@gmail.com>
2018-09-27 12:46:57 -0500

    calculate elevation changes on the route

--- CapstoneProject/ClientApp/src/components/_user/_routes/MapContainer.js
+++ CapstoneProject/ClientApp/src/components/_user/_routes/MapContainer.js
@@ -11,11 +11,16 @@ export class MapContainer extends Component {
             hasClicked: false,
             updateLine: false,
             distances: [],
-            totalDistance: 0
+            totalDistance: 0,
+            elevations: [{up: 0, down: 0}],
+            totalElevationGain: 0,
+            totalElevationLoss: 0,
+            hasElevation: false
         }
 
         this.deleteLast = this.deleteLast.bind(this);
         this.deleteAll = this.deleteAll.bind(this);
+        this.calculateElevationValues = this.calculateElevationValues.bind(this);   
     }
 
     calculateDistanceOnAdd(newCoord, currentPath) {
@@ -26,22 +31,84 @@ export class MapContainer extends Component {
         return distance/1609.344;
     }
 
+    getElevationFromGoogle(newCoord, currentPath) {
+        var lastCoord = currentPath[currentPath.length - 1];
+        let coord1 = new window.google.maps.LatLng(newCoord.lat, newCoord.lng);
+        let coord2 = new window.google.maps.LatLng(lastCoord.lat, lastCoord.lng);
+        var elevatorService = new window.google.maps.ElevationService;
+        return elevatorService.getElevationAlongPath({
+            'path': [coord1,coord2],
+            'samples': 4
+        }, this.calculateElevationValues);
+    }
+
+    getElevationChangeForEachPoint(elevations,status) {
+        let elevationVals = elevations.map(a => a.elevation.toFixed(4));
+        let compare = elevations.map(a => a.elevation.toFixed(4));
+        compare.unshift(elevationVals[0]);
+        compare.pop();
+        let elevationChanges = elevationVals.map((a, i) => {
+            return (a - compare[i]).toFixed(4);
+        }
+        );
+        return elevationChanges;
+    }
+
+    getPositiveElevationChange(elevationChanges) {
+        let positives = elevationChanges.filter(a => a > 0);
+        positives = positives.map(a => Number(a));
+        let up = positives.reduce((a, b) => a + b, 0);
+        return up;
+    }
+
+    getNegativeElevationChanges(elevationChanges) {
+        let negatives = elevationChanges.filter(a => a < 0);
+        negatives = negatives.map(a => Number(a));
+        let down = negatives.reduce((a, b) => a + b, 0);
+        return down;
+    }
+    calculateElevationValues(elevations, status) {
+        let elevationChanges = this.getElevationChangeForEachPoint(elevations, status);
+        let up = this.getPositiveElevationChange(elevationChanges);
+        let down = this.getNegativeElevationChanges(elevationChanges);
+        let elevationObjects;
+        let currentGain = this.state.totalElevationGain;
+        let currentLoss = this.state.totalElevationLoss;
+        if (this.state.hasElevation === false) {
+            elevationObjects = [{ up: up, down: down }]
+        }
+        else {
+            elevationObjects = this.state.elevations;
+            elevationObjects.push({ up: up, down: down });
+        }
+        this.setState({
+            elevations: elevationObjects,
+            totalElevationGain: currentGain + up,
+            totalElevationLoss: currentLoss + down,
+            hasElevation: true
+        });
+    }
+
+   
+
     addCoordinate(t, map, coord) {
         const { latLng } = coord;
         const lat = latLng.lat();
         const lng = latLng.lng();
         const newCoord = { lat: lat, lng: lng };
         var currentPath = this.state.coordinates;
-        var distance = this.calculateDistanceOnAdd(newCoord, currentPath );
+        var distance = this.calculateDistanceOnAdd(newCoord, currentPath);
+        
         if (this.state.hasClicked === false) {
             this.setState({
                 coordinates: [newCoord],
                 hasClicked: true,
                 updateLine: false,
-               
             });
         }
         else {
+            this.getElevationFromGoogle(newCoord, currentPath);
+            
             var currentDistanceArray = this.state.distances;
             var currentTotal = this.state.totalDistance + distance;
             currentDistanceArray.push(distance);
@@ -52,6 +119,7 @@ export class MapContainer extends Component {
                 distances: currentDistanceArray,
                 totalDistance: currentTotal
             })
+            
         }
     }
 
@@ -64,8 +132,35 @@ export class MapContainer extends Component {
                     coordinates: [{}],
                     hasClicked: false,
                     distances: [],
-                    totalDistance: 0
-
+                    totalDistance: 0,
+                    elevations: [{ up: 0, down: 0 }],
+                    totalElevationGain: 0,
+                    totalElevationLoss: 0,
+                    hasElevation: false
+                });
+            }
+            else if (this.state.coordinates.length === 2) {
+                var currentPath = this.state.coordinates;
+                currentPath.pop();
+                let currentDistance = this.state.totalDistance;
+                let currentDistanceArray = this.state.distances;
+                let removed = currentDistanceArray.pop();
+                let newDistance = currentDistance - removed;
+                let currentElevations = this.state.elevations;
+                let removedElevation = currentElevations.pop();
+                let currentUp = this.state.totalElevationGain;
+                let currentDown = this.state.totalElevationLoss;
+                let newUp = currentUp - removedElevation.up;
+                let newDown = currentDown - removedElevation.down;
+                this.setState({
+                    coordinates: currentPath,
+                    updateLine: true,
+                    totalDistance: newDistance,
+                    distances: currentDistanceArray,
+                    elevations: currentElevations,
+                    totalElevationGain: newUp,
+                    totalElevationLoss: newDown,
+                    hasElevation: false
                 });
             }
             else {
@@ -75,11 +170,20 @@ export class MapContainer extends Component {
                 let currentDistanceArray = this.state.distances;
                 let removed = currentDistanceArray.pop();
                 let newDistance = currentDistance - removed;
+                let currentElevations = this.state.elevations;
+                let removedElevation = currentElevations.pop();
+                let currentUp = this.state.totalElevationGain;
+                let currentDown = this.state.totalElevationLoss;
+                let newUp = currentUp - removedElevation.up;
+                let newDown = currentDown - removedElevation.down;
                 this.setState({
                     coordinates: currentPath,
                     updateLine: true,
                     totalDistance: newDistance,
-                    distances: currentDistanceArray
+                    distances: currentDistanceArray,
+                    elevations: currentElevations,
+                    totalElevationGain: newUp,
+                    totalElevationLoss: newDown
                 });
             }
         }
@@ -92,7 +196,11 @@ export class MapContainer extends Component {
                 coordinates: [{}],
                 hasClicked: false,
                 totalDistance: 0,
-                distances: []
+                distances: [],
+                elevations: [{ up: 0, down: 0 }],
+                totalElevationGain: 0,
+                totalElevationLoss: 0,
+                hasElevation: false
             });
             
         }
@@ -109,7 +217,18 @@ export class MapContainer extends Component {
         else if (this.state.hasClicked === true) {
             polyline = <Polyline path={this.state.coordinates} />
         }
+        var uphill;
+        var downhill;
+        if (this.state.hasElevation === true) {
+            uphill = this.state.elevations[this.state.elevations.length - 1].up.toFixed(2);
+            downhill = this.state.elevations[this.state.elevations.length - 1].down.toFixed(2);
         
+        }
+        else {
+            uphill = 0;
+            downhill = 0;
+        }
+
         return (    
             <Row>
                 <Col md={7}>
@@ -132,8 +251,16 @@ export class MapContainer extends Component {
                 </Col>
                 <Col md={3}>
                     <FormGroup>
-                        <ControlLabel>Distance</ControlLabel>
+                        <ControlLabel>Total Distance</ControlLabel>
                         <Well>{this.state.totalDistance.toFixed(3)} miles</Well>
+                        <ControlLabel>Total Elevation Gain</ControlLabel>
+                        <Well>{this.state.totalElevationGain.toFixed(2)} meters</Well>
+                        <ControlLabel>Total Elevation Loss</ControlLabel>
+                        <Well>{this.state.totalElevationLoss.toFixed(2)} meters</Well>
+                        <ControlLabel>Uphill on Last Stretch</ControlLabel>
+                        <Well>{uphill} meters</Well>
+                        <ControlLabel>Downhill on Last Stretch</ControlLabel>
+                        <Well>{downhill} meters</Well>
                     </FormGroup>
                     </Col>
                 </Row>

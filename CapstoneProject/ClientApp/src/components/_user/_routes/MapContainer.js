8e62475ec9bd9cded57c0eede1646d22e658f19c
alynielson <nielsonaly@gmail.com>
2018-09-26 22:11:33 -0500

    ability to drop points on map, undo, and clear all

--- CapstoneProject/ClientApp/src/components/_user/_routes/MapContainer.js
+++ CapstoneProject/ClientApp/src/components/_user/_routes/MapContainer.js
@@ -1,5 +1,5 @@
 ﻿import React, { Component } from 'react';
-import { Button, Form, FormGroup, FormControl, ControlLabel, Col, ColProps, Row, ButtonToolbar } from 'react-bootstrap';
+import { Button, Form, FormGroup, FormControl, ControlLabel, Col, ColProps, Row, ButtonGroup } from 'react-bootstrap';
 import { GoogleApiWrapper, Map, Polyline, DrawingManager } from 'google-maps-react'
 
 
@@ -7,47 +7,102 @@ export class MapContainer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            lineCoordinates: []
+            coordinates: [{}],
+            hasClicked: false,
+            updateLine: false
         }
-        this.initMap = this.initMap.bind(this);
+
+        this.deleteLast = this.deleteLast.bind(this);
+        this.deleteAll = this.deleteAll.bind(this);
+    }
+
+    addCoordinate(t, map, coord) {
+        const { latLng } = coord;
+        const lat = latLng.lat();
+        const lng = latLng.lng();
+        const newCoord = { lat: lat, lng: lng };
+        var currentPath = this.state.coordinates;
+        if (this.state.hasClicked === false) {
+            this.setState({
+                coordinates: [newCoord],
+                hasClicked: true,
+                updateLine: false
+            });
+        }
+        else {
+            var newPath = currentPath.concat(newCoord);
+            this.setState({
+                coordinates: newPath,
+               updateLine: false
+            })
+        }
     }
 
    
 
-
-
-    initMap(mapProps, map) {
-        var self = this;
-        const { google } = mapProps;
-        
-
-        const drawingManager = new google.maps.drawing.DrawingManager({
-            drawingMode: 'polyline',
-            drawingControl: true,
-            drawingControlOptions: {
-                position: google.maps.ControlPosition.TOP_CENTER,
-                drawingModes: [
-                    google.maps.drawing.OverlayType.POLYLINE,
-                ]
-            },
-            map: map
-        });
+   deleteLast() {
+        if (this.state.hasClicked === true) {
+            if (this.state.coordinates.length === 1) {
+                this.setState({
+                    coordinates: [{}],
+                    hasClicked: false,
+                    
+                    
+                });
+            }
+            var currentPath = this.state.coordinates;
+            currentPath.pop();
+            this.setState({
+                coordinates: currentPath,
+                updateLine: true
+            });
+        }
     }
 
+    
+    deleteAll() {
+        if (this.state.hasClicked === true) {
+            this.setState({
+                coordinates: [{}],
+                hasClicked: false,
+            });
+            
+        }
+    }
 
     render() {
+        var polyline;
+        if (this.state.hasClicked == false) {
+            polyline = null
+        }
+        else if (this.state.updateLine === true) {
+            polyline = <Polyline path={this.state.coordinates} />
+        }
+        else if (this.state.hasClicked === true) {
+            polyline = <Polyline path={this.state.coordinates} />
+        }
         
-        return (
+        return (    
+            <Row>
+                <Col md={10}>
             <div className='map'>
-                <Map google={window.google}
-                    onReady={this.initMap}
-                    center={{ lat: this.props.lat, lng: this.props.lng }}
-                    zoom={14}
-                    onClick={this.addPoint}
+                        <Map google={window.google}
+                            center={{ lat: this.props.lat, lng: this.props.lng }}
+                            zoom={14}
+                            onClick={(t, map, coord) => this.addCoordinate(t, map, coord)}
                 >
-
+                            {polyline}
+                            
                 </Map>
-            </div>
+                    </div>
+                </Col>
+                <Col md={2}>
+            <ButtonGroup vertical>
+                                <Button onClick={this.deleteLast}>Undo</Button>
+                                <Button onClick={this.deleteAll}>Clear All</Button>
+                            </ButtonGroup>
+                    </Col>
+                </Row>
             );
     }
 }

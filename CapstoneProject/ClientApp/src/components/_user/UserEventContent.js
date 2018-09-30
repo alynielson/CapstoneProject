7a50d436cef311dbb2028a85340224735fdbabba
alynielson <nielsonaly@gmail.com>
2018-09-30 09:10:24 -0500

    page for creating events

--- CapstoneProject/ClientApp/src/components/_user/UserEventContent.js
+++ CapstoneProject/ClientApp/src/components/_user/UserEventContent.js
@@ -1,24 +1,49 @@
 ﻿import React, { Component } from 'react';
 import { Button, Form, FormGroup, FormControl, ControlLabel, Col, ColProps, Row, ButtonToolbar } from 'react-bootstrap';
 import { Route, Link, Redirect, withRouter, BrowserRouter } from 'react-router-dom';
+import { CreateEvent } from './_events/CreateEvent';
 
 
 export class UserEventContent extends Component {
     constructor(props) {
         super(props);
-
+        this.state = {
+            createEvent: false
+        }
+        this.addNewEvent = this.addNewEvent.bind(this);
+        this.backToEventHome = this.backToEventHome.bind(this);
+    }
+    addNewEvent(event) {
+        event.preventDefault();
+        this.setState({
+            createEvent: true
+        })
+    }
+    backToEventHome() {
+        this.setState({
+            createEvent: false
+        })
     }
 
 
 
-
     render() {
-        return (
+        const backToEventHome = this.backToEventHome;
+        if (this.state.createEvent) {
+            return (<CreateEvent backToEventHome={backToEventHome} />);
+        }
+        else {
+            return (
 
-            <div> User Event Content </div>
+                <div>
+                    <Button onClick={(event) => this.addNewEvent(event)}>Create New Event</Button>
 
 
-        );
+                </div>
+
+
+            );
+        }
     }
 
 
---
+++ CapstoneProject/ClientApp/src/components/_user/_events/CreateEvent.js
@@ -0,0 +1,32 @@
+﻿import React, { Component } from 'react';
+import { Button, Form, FormGroup, FormControl, ControlLabel, Col, ColProps, Row, ButtonToolbar } from 'react-bootstrap';
+import { Route, Link, Redirect, withRouter, BrowserRouter } from 'react-router-dom';
+
+export class CreateEvent extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        var action;
+        if (!this.state.hasGroups) {
+            action = <SelectGroups />
+        }
+        return (
+            <div>
+                <h2>Create Event</h2>
+                <Row>
+                    {action}
+                </Row>
+                <Row>
+                    <Col md={3}>
+                        <Button onClick={this.props.backToEventHome}>Back</Button>
+                    </Col>
+                </Row>
+            </div>
+            );
+    }
+
+
+
+}

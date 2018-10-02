80352b073e8e074193db0f6a452aa897c2e7b01f
alynielson <nielsonaly@gmail.com>
2018-10-02 13:42:41 -0500

    remove unused code in userscontroller

--- CapstoneProject/Controllers/UsersController.cs
+++ CapstoneProject/Controllers/UsersController.cs
@@ -2,14 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using CapstoneProject.ViewModels;
 using Newtonsoft.Json;
 using CapstoneProject.Models;
 using Microsoft.AspNetCore.Routing;
-using System.Security.Cryptography;
-using Microsoft.IdentityModel.Protocols;
 using CapstoneProject.Operations;
 using IntegrationProject.Data;
 using System.IO;
@@ -252,13 +249,6 @@ namespace CapstoneProject.Controllers
             
         }
 
-        [HttpGet("[action]")]
-        public IEnumerable<UserSearchVM> SearchUsersByAll(string city, string state, string first_name = null, string last_name = null)
-        {
-            IEnumerable<User> nameMatches = SearchByName(first_name, last_name);
-            IEnumerable<User> matches = nameMatches.Where(a => a.City == city && a.State == state);
-            return GetSearchMatches(matches);
-        }
 
         [HttpGet("[action]")]
         public IEnumerable<UserSearchVM> UniversalUserSearch(string term1)
@@ -297,17 +287,5 @@ namespace CapstoneProject.Controllers
             return location;
         }
 
-
-        // PUT: api/Users/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }

fa2f90848bd6e08dd3d18fefe36f6ae9a93bdef5
alynielson <nielsonaly@gmail.com>
2018-10-17 14:35:48 -0500

    style route info component

--- CapstoneProject/ClientApp/src/components/_user/_routes/RouteInfo.js
+++ CapstoneProject/ClientApp/src/components/_user/_routes/RouteInfo.js
@@ -6,5 +6,19 @@ export class RouteInfo extends Component {
         super(props);
 
     }
-
+    render() {
+        const infoBox = {
+            backgroundColor: "#c2e6ff",
+            marginTop: "10px",
+            paddingLeft: "15px",
+            paddingRight: "15px",
+            color: "#555",
+            fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
+            overflow: "auto",
+            marginBottom: "10px",
+            boxShadow: "4px 4px 5px 0px rgba(0,0,0,0.41)",
+            borderRadius: "5px"
+        }
+        return ();
+    }
 }
